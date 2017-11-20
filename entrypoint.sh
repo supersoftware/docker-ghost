00a933c783fc91df04f50fce03aeb885d8ff454e
yamazawa <yamazawa@tokyo.supersoftware.co.jp>
2017-11-20 19:11:51 +0900

    fix extract path

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ cp -pr node_modules/ghost-storage-adapter-s3 content/adapters/storage/s3
 # custom theme
 mkdir -p /var/lib/ghost/content/themes/ghostium-2.3.2-custom
 curl -SLO https://github.com/supersoftware/ghostium/archive/v2.3.2-custom.tar.gz
-tar xzf v2.3.2-custom.tar.gz -C /var/lib/ghost/versions/1.17.3/content/themes/ghostium-2.3.2-custom
+tar xzf v2.3.2-custom.tar.gz -C /var/lib/ghost/content/themes/ghostium-2.3.2-custom
 rm v2.3.2-custom.tar.gz
 
 # execute the default entrypoint
