4ba5e6752b1786626910804d81d605e83acf813f
yamazawa <yamazawa@tokyo.supersoftware.co.jp>
2017-11-20 21:43:51 +0900

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ cp -pr node_modules/ghost-storage-adapter-s3 content/adapters/storage/s3
 # custom theme
 mkdir -p /var/lib/ghost/content/themes/ghostium-custom
 curl -SLO https://github.com/supersoftware/ghostium/archive/v2.4.0-custom.tar.gz
-tar xzf v2.4.0-custom.tar.gz -C /var/lib/ghost/content/themes
+tar xzf v2.4.0-custom.tar.gz -C /var/lib/ghost/content/themes && mv ghostium-2.4.0-custom ghostium-custom
 rm v2.4.0-custom.tar.gz
 
 # execute the default entrypoint
