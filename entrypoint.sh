0834c816092bd757029cab0edd3b0edb2518c6df
Ken Fukuyama <fukuyama@supersoftware.co.jp>
2017-12-06 13:02:10 +0900

    fixed issue with duplicate meta tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,19 @@
 set -e
 
 # make sure the s3 adapter is in adapters directory
-mkdir -p content/adapters/storage
-cp -pr node_modules/ghost-storage-adapter-s3 content/adapters/storage/s3
+##mkdir -p content/adapters/storage
+##cp -pr node_modules/ghost-storage-adapter-s3 content/adapters/storage/s3
 
 # custom theme
-mkdir -p /var/lib/ghost/content/themes
-cd /var/lib/ghost/content/themes
-curl -SLO https://github.com/supersoftware/ghostium/archive/v2.4.4-custom.zip
-unzip v2.4.4-custom.zip
-mv ghostium-2.4.4-custom ghostium-custom
-rm v2.4.4-custom.zip
-cd /var/lib/ghost
+THEME_VER=2.4.5-custom
+##mkdir -p /var/lib/ghost/content/themes
+##cd /var/lib/ghost/content/themes
+curl -SLO https://github.com/supersoftware/ghostium/archive/v${THEME_VER}.zip
+unzip v${THEME_VER}.zip
+mv ghostium-${THEME_VER} ghostium-custom
+##rm v${THEME_VER}.zip
+##cd /var/lib/ghost
 
 # execute the default entrypoint
-docker-entrypoint.sh "$@"
+##docker-entrypoint.sh "$@"
 
