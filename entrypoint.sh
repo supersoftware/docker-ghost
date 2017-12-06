1f195e795a9cc53fd2382791c886d49df3f9d24a
Ken Fukuyama <fukuyama@supersoftware.co.jp>
2017-12-06 13:12:32 +0900

    fixed entrypoint. missed comment outing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,19 @@
 set -e
 
 # make sure the s3 adapter is in adapters directory
-##mkdir -p content/adapters/storage
-##cp -pr node_modules/ghost-storage-adapter-s3 content/adapters/storage/s3
+mkdir -p content/adapters/storage
+cp -pr node_modules/ghost-storage-adapter-s3 content/adapters/storage/s3
 
 # custom theme
 THEME_VER=2.4.5-custom
-##mkdir -p /var/lib/ghost/content/themes
-##cd /var/lib/ghost/content/themes
+mkdir -p /var/lib/ghost/content/themes
+cd /var/lib/ghost/content/themes
 curl -SLO https://github.com/supersoftware/ghostium/archive/v${THEME_VER}.zip
 unzip v${THEME_VER}.zip
 mv ghostium-${THEME_VER} ghostium-custom
-##rm v${THEME_VER}.zip
-##cd /var/lib/ghost
+rm v${THEME_VER}.zip
+cd /var/lib/ghost
 
 # execute the default entrypoint
-##docker-entrypoint.sh "$@"
+docker-entrypoint.sh "$@"
 
